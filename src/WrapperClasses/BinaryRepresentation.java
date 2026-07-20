01e247722db89119fac6619c9bec66ef09e79f2d
Keerthii23 <keerthichennupalli@gmail.com>
2026-07-20 22:28:22 +0530

    Added wrapperClass Programs

---
+++ src/WrapperClasses/BinaryRepresentation.java
@@ -0,0 +1,21 @@
+package WrapperClasses;
+
+import java.util.Scanner;
+
+public class BinaryRepresentation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        String binary = Integer.toBinaryString(num);
+
+        binary = String.format("%8s", binary).replace(' ', '0');
+
+        System.out.println(binary);
+
+        sc.close();
+    }
+}
